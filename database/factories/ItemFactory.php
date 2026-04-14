c2c1cd028517db1a4f3b52a197a0238cb73de115
Ahmad Sukron Ramadhan <ahmadsukronramadhan03@gmail.com>
2026-04-14 07:38:09 +0700

    Add MenuController for managing menu display and cart functionality

--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -18,7 +18,14 @@ class ItemFactory extends Factory
             'category_id' => $this->faker->numberBetween(1, 2),
             'price' => $this->faker->randomFloat(2, 1000, 100000),
             'description' => $this->faker->text(),
-            'img' => $this->faker->imageUrl(),
+            'img' => $this->faker->randomElement([
+                'https://images.unsplash.com/photo-1579871494447-9811cf80d66c', // Sushi Nigiri
+                'https://images.unsplash.com/photo-1553621042-f6e147245754', // Sushi Roll
+                'https://images.unsplash.com/photo-1569718212165-3a8278d5f624', // Ramen Bowl
+                'https://images.unsplash.com/photo-1591814447921-7cf7a5c6adbc', // Miso Ramen
+                'https://images.unsplash.com/photo-1582450871972-ab5ca641643d', // Gyoza
+                'https://images.unsplash.com/photo-1558985250-27a406d64cb3', // Takoyaki
+            ]),
             'is_active' => $this->faker->boolean(),
         ];
     }
